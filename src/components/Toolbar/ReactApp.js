a8678475708368d93c8f5b98dc04fe4767ca66e7
Muhammed <muhammed10200802@outlook.com>
2020-01-30 20:05:05 -0800

    first commit

---
+++ src/components/Toolbar/ReactApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ReactApp.css'
+
+class ReactApp extends React.Component {
+    render () {
+        return  <div className = 'sadrzaj2'>ReactApp</div>;
+    }
+}
+export default ReactApp;
---
+++ src/components/Toolbar/Start.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Start.css';
+
+class Start extends React.Component{
+    render () {
+        return <div className = 'sadrzaj1'>Start</div>
+    }
+}
+export default Start;
